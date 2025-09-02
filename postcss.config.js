4e73db611b91c1062890e5d849867a0e927a9b3d
orjueladavid51@gmail.com <cp131537@gmail.com>
2025-09-02 10:48:49 -0500

    subiendo act cartas

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,5 @@
 export default {
   plugins: {
-    "@tailwindcss/postcss": {},
-    tailwindcss: {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {},
   },
 };
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,14 @@
-export default function Card({imagen, titulo, descripcion, onClick}) {
-
-
-
-
-    return (
-
-        <div className="bg-white rounded-lg shadow-md overflow-hidden cursor-pointer transition-transform transform duration-300 ease-in-out hover:scale-105 hover:shadow-lg" onClick={onClick}>
-            <img src={imagen} alt={titulo} className="w-full h-48 object-cover" />
-            <div className="p-4">
-                <h3 className="text-lg font-bold">{titulo}</h3>
-                <p className="text-gray-600">{descripcion}</p>
-            </div>
-        </div>
-    )
-
+export default function Card({ imagen, titulo, descripcion, onClick }) {
+  return (
+    <div
+      onClick={onClick}
+      className="bg-white rounded-lg shadow-md overflow-hidden cursor-pointer transform transition duration-300 hover:shadow-xl"
+    >
+      <img src={imagen} alt={titulo} className="w-full h-48 object-cover" />
+      <div className="p-4">
+        <h3 className="text-lg font-bold">{titulo}</h3>
+        <p className="text-gray-600">{descripcion}</p>
+      </div>
+    </div>
+  );
 }
